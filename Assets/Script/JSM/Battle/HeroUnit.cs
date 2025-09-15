2f69893e21fb4f4474657b3b6efbaa8928eee6d2
ParkMinsoo <harimaho88@gmail.com>
2025-09-15 18:47:36 +0900

    setting

--- Assets/Script/JSM/Battle/HeroUnit.cs
+++
@@ -1,8 +0,0 @@
-public class HeroUnit : Unit
-{
-    public override void Initialize()
-    {
-        base.Initialize();
-        if(stats.SkillID!=null&& stats.SkillID.Count>=2) SkillManager.Instance.UseSkill(stats.SkillID[1], this.isEnemy);
-    }
-}
